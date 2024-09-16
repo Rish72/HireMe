a9d9f44b157d6d8676385bbc467abb2005d875d3
Rishabh <rishabhgoel2020@gmail.com>
2024-09-16 20:43:50 +0530

    Linked Supabase waiting to test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LogoImg from "/hireme.png"
   import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
+import { SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/clerk-react'
 
 const Header = () => {
   return (
@@ -11,6 +12,20 @@ const Header = () => {
       <img src={LogoImg} alt="Logo of website" className='h-10'/>  
     </Link>
     <Button variant="outline"> Login </Button>
+    
+    
+    
+    
+    
+    
+    
+    
+    {/* <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn> */}
     </nav>
     </>
   )
